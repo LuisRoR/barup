feb0a13f2ebaab6293dce016b12a8b75930c86c7
Luis Martinez Peraza <luismartinezperaza@gmail.com>
2018-11-01 19:57:02 -0600

    NEW Deployment

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 # TESTING BARUP app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost/blogz'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost/barup'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATBASE_URL'] #'postgres://postgres@localhost/barup'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['POSTS_PER_PAGE'] = 3
